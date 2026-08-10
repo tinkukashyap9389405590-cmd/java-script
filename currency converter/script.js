392dc1128294fa277e7e88834c9de25f569008d2
Tinku kashyap <tinkukashyap9389405590@gmail.com>
2026-08-10 23:09:32 +0530

    uhouhoh

--- currency converter/script.js
+++ currency converter/script.js
@@ -1,27 +1,45 @@
 const BASE_URL= "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
-const dropdown=document.querySelectorAll("dropdown selecct");
-for(code in countryList){
-    console.log(code);
+const dropdowns =document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+
+for(let select of dropdowns){
+    for(currCode in countryList){
+        let newOption = document.createElement("option");     
+        newOption.innerText=currCode;   
+        newOption.value=currCode;
+          if(select.name === "from" && currCode === "USD"){
+            newOption.selected="selected";
+        } else if(select.name === "to" && currCode === "INR"){
+            newOption.selected="selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlage(evt.target);
+    });
 }
-
-// for(let select of dropdown){
-//     for(let curr in countryList){
-//         let newOption=document.creatElement("option");     
-//         newOption.innerText=currCode;   
-//         newOption.value=currCode;
-//         if(select.name === "from" && currCode === "USD"){
-//             newOption.selected="selected";
-//         } else if(select.name === "to" && currCode === "USD"){
-//             newOption.selected="selected";
-//         }
-//         select.append(newOption);
-//     }
-//     select.addListener("change",(evt) =>{
-//     updateFlage(evt.targate);
-// });
-// }
-// const updateFlage=(Element)=>{
-//     console.log(Elment);
-// };
-
+const updateFlage = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtvalue = amount.value;
+    if (amtvalue == "" || amtvalue < 1) {
+        amtvalue = 1;
+        amount.value = "1";
+    }
+    // const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    // let response = await fetch(URL);
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    console.log(response);
+});
+      
 
